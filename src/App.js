333d59aa313c09edd15c58d561de927d35500562
Abriana Duran <abrianamduran@gmail.com>
2021-04-26 13:33:58 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import List from './List'
 import './App.css';
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+  return <main>
+    <header>
+      <h1>Trelloyes</h1>
+    </header>
+    <div className="App-list">
+      {store.lists.map(list => (
+        <List key={list.id} header={list.header} cards={list.cardIds.map(id => store.allCards[id])}></List>
+      ))}
+
     </div>
-  );
+  </main>
+  
+  }
+  
 }
 
 export default App;
+
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './card.css';
+import './List.js';
+
+function Card(props){
+    const title = props.title;
+    const content = props.content;
+    return (
+        <div className="Card">
+            <button type='button'>delete</button>
+            <h3>{title}</h3>
+            <p>{content}</p>
+        </div>
+    )
+}
+
+export default Card;
+
